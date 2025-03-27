277e328093acfa95b2c46c705add6b7dfd15fcb5
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2025-03-27 18:42:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
-app = Client("my_bot", api_id=25637343, api_hash="70fb79a89ec2d30cab05704e817e5be6", bot_token="BOT_TOKEN")
+app = Client("my_bot", api_id=25637343, api_hash="70fb79a89ec2d30cab05704e817e5be6", bot_token="8070188741:AAGyFHFlZP08ZbdaFQrV70KZTB8mvf5ql5c")
 
 @app.on_message(filters.photo)
 async def get_photo_id(client, message):
